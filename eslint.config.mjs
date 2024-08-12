91f80d014034e170b812893798aaec8b434d4553
David Gomez <1001.28113626.ucla@gmail.com>
2024-08-12 12:11:58 -0400

    testing eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,10 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.{,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      'no-undef': 'off',  // Desactiva la regla no-undef
-      // otras reglas específicas que podrían causar problemas
-    },
-  },
+  
 ];
