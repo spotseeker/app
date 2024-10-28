6357818c29e7a5fd025c95ccdc58146174a3f259
Andres Alvarez <andres15alvarez@gmail.com>
2024-10-28 13:02:56 -0400

    Create fixtures with images

---
+++ src/fixtures/images.ts
@@ -0,0 +1,11 @@
+export const images = [
+    {
+        url: "https://spotseeker.sirv.com/images/Rectangle%206.png"
+    },
+    {
+        url: "https://spotseeker.sirv.com/images/image%2010.png"
+    },
+    {
+        url: "https://spotseeker.sirv.com/images/image%2011.png"
+    }
+]
