5cc302737ca3a666119c89d8f4a08be760b34f51
Andres Alvarez <andres15alvarez@gmail.com>
2024-10-28 13:02:36 -0400

    Create image grid component for search view

---
+++ src/components/ImageGrid.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+
+type ImageGridProps = {
+    url: string
+}
+
+const style = StyleSheet.create({
+    image: {
+        width: 120,
+        height: 120
+    }
+})
+
+function ImageGrid({item}: {item: ImageGridProps}){
+    return (
+        <Image
+            style={style.image}
+            source={{uri: item.url}}
+        />
+    )
+}
+
+export default ImageGrid;
