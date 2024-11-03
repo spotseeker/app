639e566e9eded1af366dec61ceebe7c1ad22ed62
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-03 12:41:43 -0400

    Add map component

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -31,7 +31,7 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
-        name="MapView"
+        name="map"
         options={{
           headerShown: false,
           title: '',
---
+++ src/app/(tabs)/map.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import GetLocation from 'react-native-get-location'
+
+let latitude: number
+let longitude: number
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
+
+GetLocation.getCurrentPosition({
+  enableHighAccuracy: true,
+  timeout: 60000,
+})
+.then(location => {
+  console.log(location);
+  latitude = location.latitude
+  longitude = location.longitude
+})
+.catch(error => {
+  const { code, message } = error;
+  console.warn(code, message);
+})
+
+export default () => (
+  <View style={styles.container}>
+     <MapView
+       provider={PROVIDER_GOOGLE}
+       style={styles.map}
+       region={{
+         latitude: latitude,
+         longitude: longitude,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+  </View>
+);
