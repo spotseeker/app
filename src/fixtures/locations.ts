7dc6f89d8af503c330c5b903403c878a8212f999
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-06 16:16:02 -0400

    Create locations fixture

---
+++ src/fixtures/locations.ts
@@ -0,0 +1,18 @@
+export const locations = [
+  {
+    id: 1,
+    text: 'Barquisimeto'
+  },
+  {
+    id: 2,
+    text: 'Caracas'
+  },
+  {
+    id: 3,
+    text: 'Valencia'
+  },
+  {
+    id: 4,
+    text: 'Maracaibo'
+  }
+]
