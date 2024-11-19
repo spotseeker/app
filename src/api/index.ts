9c25c22d52aa3d1b6f3cd211bb4e4daa661ec110
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-19 19:14:10 -0400

    Include comment service in main api class

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { AuthService } from './auth'
+import { CommentService } from './comment'
 import { PostService } from './post'
 import { UserService } from './user'
 
@@ -6,10 +7,12 @@ export class SpotSeekerAPI {
   auth: AuthService
   user: UserService
   post: PostService
+  comment: CommentService
 
   constructor() {
     this.auth = new AuthService()
     this.user = new UserService()
     this.post = new PostService()
+    this.comment = new CommentService()
   }
 }
